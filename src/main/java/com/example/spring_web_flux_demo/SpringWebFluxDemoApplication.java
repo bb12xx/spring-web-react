4a7c81d738ee2d7a8334d7c5acfedab5ba03e34e
bb12xx <bristibhuyan@gmail.com>
2026-09-13 16:53:13 +0530

    My first commit

---
+++ src/main/java/com/example/spring_web_flux_demo/SpringWebFluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_flux_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_web_flux_demo/SpringWebFluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_flux_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
